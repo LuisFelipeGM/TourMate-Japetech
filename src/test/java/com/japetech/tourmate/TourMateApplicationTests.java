d75a0990fcc93c42f86c0ebd4bf27ee10d223097
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-05-03 08:08:30 -0300

    Primeiro Commit, Add Dependencias

---
+++ src/test/java/com/japetech/tourmate/TourMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japetech.tourmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

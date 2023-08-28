1be0047fef87082bce1685119b7e993fe675a6b5
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-08-28 20:46:53 -0300

    Primeiro commit, Add Dependencias

---
+++ src/main/java/com/japetech/tourmate/TourMateApplication.java
@@ -0,0 +1,13 @@
+package com.japetech.tourmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourMateApplication.class, args);
+	}
+
+}
